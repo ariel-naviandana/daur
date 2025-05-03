3858549e4d9c5399019eeb5456d1cf5b8677be4c
Ariel2711 <arielnaviandanaputra@gmail.com>
2025-05-03 19:05:36 +0700

    add migration

---
+++ database/migrations/2025_05_03_120012_create_recycle_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recycle_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('bank_id')->nullable()->constrained()->onDelete('set null');
+            $table->text('pickup_address')->nullable();
+            $table->enum('method', ['pickup', 'dropoff']);
+            $table->enum('status', ['waiting', 'process', 'cancel', 'success'])->default('waiting');
+            $table->decimal('total_weight', 10, 2)->nullable();
+            $table->decimal('total_amount', 12, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recycle_transactions');
+    }
+};
---
+++ database/migrations/2025_05_03_120031_create_recycle_transaction_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recycle_transaction_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained('recycle_transactions')->onDelete('cascade');
+            $table->foreignId('waste_type_id')->constrained()->onDelete('cascade');
+            $table->decimal('quantity', 10, 2);
+            $table->decimal('sub_total', 12, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recycle_transaction_items');
+    }
+};
