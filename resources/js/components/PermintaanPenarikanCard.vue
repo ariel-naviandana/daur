d27867ee100e598f55f2876d8d6a26171c7ec66a
stargazer <stargazer>
2025-06-02 17:59:38 +0700

    Withdrawal client functional

---
+++ resources/js/components/PermintaanPenarikanCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div v-for="(item, index) in permintaan" :key="index" class="card">
+            <p><strong>{{ item.nama }}</strong></p>
+            <p>{{ item.metode }}</p>
+            <p>Rp. {{ item.jumlah.toLocaleString() }}</p>
+            <div class="actions">
+                <button @click="setujui(index)">Setujui</button>
+                <button @click="tolak(index)">Tolak</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const permintaan = ref([
+    { nama: 'Walter White', metode: 'Dana 0812...', jumlah: 43000 },
+    // data dummy lainnya
+])
+
+function setujui(index) {
+    // implementasi logika menyetujui
+    console.log('Setujui:', permintaan.value[index])
+}
+
+function tolak(index) {
+    // implementasi logika menolak
+    console.log('Tolak:', permintaan.value[index])
+}
+</script>
+
+<style scoped>
+.actions {
+    margin-top: 10px;
+}
+button {
+    margin-right: 10px;
+    padding: 5px 10px;
+}
+</style>
---
+++ resources/js/components/SaldoKeluarCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div :style="transactionRow">
+        <div :style="transactionIcon">
+            <img src="/images/ic-transaction.svg">
+        </div>
+        <div :style="transactionInfo">
+            <p :style="transactionNameInfo">{{ item.nama }}</p>
+            <p :style="transactionDate">{{ item.tanggal }}</p>
+            <p :style="transactionDetail">Transfer ke: {{ item.metode }}</p>
+            <p :style="transactionStatus">Status: {{ item.status }}</p>
+        </div>
+        <p :style="mutasiAmountMinusStyle">- Rp. {{ item.jumlah.toLocaleString() }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({ item: Object })
+
+const transactionRow = { /* ... */ }
+const transactionIcon = { /* ... */ }
+const transactionInfo = { /* ... */ }
+const transactionNameInfo = { /* ... */ }
+const transactionDate = { /* ... */ }
+const transactionDetail = {
+    fontSize: '0.8rem',
+    marginTop: '4px',
+    color: '#666'
+}
+const transactionStatus = {
+    fontSize: '0.8rem',
+    color: '#FF9800', // warna oranye utk status proses
+    marginTop: '2px'
+}
+const mutasiAmountMinusStyle = {
+    color: '#F44336',
+    fontWeight: 'bold',
+    fontSize: '1rem'
+}
+</script>
