c934959dc09b33ad723655aa9c3f4b995b9d16a9
ariel-naviandana <arielnaviandanaputra@gmail.com>
2025-06-04 09:14:11 +0700

    update config

---
+++ config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Paths
+    |--------------------------------------------------------------------------
+    |
+    | Most templating systems load templates from disk. Here you may specify
+    | an array of paths that should be checked for your views.
+    |
+    */
+    'paths' => [
+        resource_path('views'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled View Path
+    |--------------------------------------------------------------------------
+    |
+    | This option determines where all the compiled Blade templates will be
+    | stored for your application. In serverless environments like Vercel,
+    | we use /tmp to ensure write access.
+    |
+    */
+    'compiled' => env('VIEW_COMPILED_PATH', '/tmp/framework/views'),
+];
