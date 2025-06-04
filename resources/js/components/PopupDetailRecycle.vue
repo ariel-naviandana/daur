e93cc61650f6c23d6276bd0dde9a5ad4389f98e5
ariel-naviandana <arielnaviandanaputra@gmail.com>
2025-06-04 12:23:17 +0700

    update marker icon

--- resources/js/components/PopupDetailRecycle.vue
+++ resources/js/components/PopupDetailRecycle.vue
@@ -167,6 +167,17 @@ import { RecycleTransactionItem } from '@/interfaces/RecycleTransactionItem'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+})
+
 const isHoverReject = ref(false)
 const isHoverAccept = ref(false)
 
--- resources/js/components/PopupFormBank.vue
+++ resources/js/components/PopupFormBank.vue
@@ -74,6 +74,17 @@ import { Bank } from '@/interfaces/Bank'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+})
+
 const OPENCAGE_API_KEY = 'e5f8659f848545f486b84de646bd104d'
 
 const isHoverCancel = ref(false)
